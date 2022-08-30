afdf31006b013c36bddfc60fa710a228c7afa064
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-30 22:29:48 +0300

    Added Add New Course button feature at Courses comp.

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Button from '../../common/Button/Button';
 import { mockedAuthorsList, mockedCoursesList } from '../../constants';
 import { Author } from '../../models/author.interface';
 import { Course } from '../../models/course.interface';
-import CourseItem from './components/CourseItem/CourseCard';
+import CourseCard from './components/CourseCard/CourseCard';
 import SearchBar from './components/SearchBar/SearchBar';
 import './Courses.scss';
 
@@ -10,15 +11,29 @@ function Courses(): JSX.Element {
   const courses: Course[] = mockedCoursesList;
   const authors: Author[] = mockedAuthorsList;
 
+  const [addCourse, setAddCourse] = useState(false);
+
+  function handleAddNewCourseClick(): void {
+    setAddCourse(!addCourse);
+  }
+
+  const courseCards: JSX.Element = (
+    <div className="Courses__cards">
+      {courses.map((course) => (
+        <CourseCard course={course} authorsList={authors} key={course.id} />
+      ))}
+    </div>
+  );
+
+  const createCourse: JSX.Element = <div>createCourse</div>;
+
   return (
     <main className="Courses">
       <div className="Courses__filter">
         <SearchBar />
-        <Button buttonText="Add New Course" />
+        <Button buttonText="Add New Course" onClick={handleAddNewCourseClick} />
       </div>
-      {courses.map((course) => (
-        <CourseItem course={course} authorsList={authors} key={course.id} />
-      ))}
+      {addCourse ? createCourse : courseCards}
     </main>
   );
 }
