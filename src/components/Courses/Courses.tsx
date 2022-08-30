92f607f035ec87b18b703fcead04710187e4c6c7
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-30 19:00:11 +0300

    Added Courses component

---
+++ src/components/Courses/Courses.tsx
@@ -0,0 +1,20 @@
+import { mockedAuthorsList, mockedCoursesList } from '../../constants';
+import { Author } from '../../models/author.interface';
+import { Course } from '../../models/course.interface';
+import CourseItem from './components/CourseItem/CourseItem';
+import './Courses.scss';
+
+function Courses(): JSX.Element {
+  const courses: Course[] = mockedCoursesList;
+  const authors: Author[] = mockedAuthorsList;
+
+  return (
+    <main className="courses">
+      {courses.map((course) => (
+        <CourseItem course={course} authorsList={authors} key={course.id} />
+      ))}
+    </main>
+  );
+}
+
+export default Courses;
