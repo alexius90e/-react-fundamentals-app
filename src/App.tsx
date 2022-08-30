640b04090294d836404269e4ddc21b655c4bb4f4
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-30 19:21:13 +0300

    Refactored App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import './App.scss';
+import Courses from './components/Courses/Courses';
+import CreateCourse from './components/CreateCourse/CreateCourse';
 import Header from './components/Header/Header';
 
 export default function App(): JSX.Element {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <CreateCourse />
+      <Courses />
+    </>
+  );
 }
