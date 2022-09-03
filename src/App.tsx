aeb79d74a7760c3506eb4114c9174ed5355ce479
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-09-03 23:28:56 +0300

    Moved BrowserRouter to App comp.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { Author } from './models/author.interface';
+import { Course } from './models/course.interface';
+import { mockedAuthorsList, mockedCoursesList } from './constants';
+
+import Courses from './components/Courses/Courses';
+import CreateCourse from './components/CreateCourse/CreateCourse';
+import CourseInfo from './components/CourseInfo/CourseInfo';
 import Header from './components/Header/Header';
 
 import './App.scss';
 
 export default function App(): JSX.Element {
+  const [courses, setCourses] = useState(mockedCoursesList as Course[]);
+  const [authors, setAuthors] = useState(mockedAuthorsList as Author[]);
+
+  const createCourse = (newCourse: Course): void => setCourses([...courses, newCourse]);
+  const createAuthor = (newAuthor: Author): Author => {
+    setAuthors([...authors, newAuthor]);
+    return newAuthor;
+  };
+
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Outlet />
-    </>
+      <Routes>
+        <Route path="/" element={<Navigate to="courses" />} />
+
+        <Route path="/courses" element={<Courses {...{ courses, authors }} />} />
+
+        <Route
+          path="/courses/add"
+          element={<CreateCourse {...{ authors, createCourse, createAuthor }} />}
+        />
+
+        <Route path="/courses:courseId" element={<CourseInfo />} />
+
+        <Route path="/login" />
+
+        <Route path="/register" />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import App from './App';
-import Courses from './components/Courses/Courses';
-import CreateCourse from './components/CreateCourse/CreateCourse';
-import CourseInfo from './components/CourseInfo/CourseInfo';
-
-import { Author } from './models/author.interface';
-import { Course } from './models/course.interface';
-import { mockedAuthorsList, mockedCoursesList } from './constants';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const courses: Course[] = mockedCoursesList;
-const authors: Author[] = mockedAuthorsList;
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="" element={<Navigate to="courses" />} />
-
-          <Route path="courses" element={<Outlet />}>
-            <Route path="" element={<Courses courses={courses} authors={authors} />} />
-
-            <Route path="add" element={<CreateCourse authors={authors} />} />
-
-            <Route path=":courseId" element={<CourseInfo />} />
-          </Route>
-
-          <Route path="login" />
-
-          <Route path="register" />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
