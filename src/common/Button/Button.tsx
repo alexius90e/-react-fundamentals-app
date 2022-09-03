ddfd7df1a6ae84d01ed18d3d94752257372499e5
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-09-03 15:07:54 +0300

    Refactored Button

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,14 +1,19 @@
+import { Link } from 'react-router-dom';
 import './Button.scss';
 
 interface ButtonProps {
   buttonText: string;
+  link?: string;
   onClick?: () => void;
 }
 
 export default function Button(props: ButtonProps): JSX.Element {
-  return (
+  const buttonElement: JSX.Element = (
     <button className="Button" onClick={props.onClick}>
       {props.buttonText}
     </button>
   );
+
+  if (props.link !== undefined) return <Link to={props.link}>{buttonElement}</Link>;
+  return buttonElement;
 }
